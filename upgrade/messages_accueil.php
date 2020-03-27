b55b427251e29b60f92f7e8e426b47ed59ccf8e7
Kostas Chatzikokolakis <kostas@chatzi.org>
2020-03-27 15:27:03 +0200

    Initial commit

---
+++ upgrade/messages_accueil.php
@@ -0,0 +1,42 @@
+<?php
+/*========================================================================
+*   Open eClass 2.3
+*   E-learning and Course Management System
+* ========================================================================
+*  Copyright(c) 2003-2010  Greek Universities Network - GUnet
+*  A full copyright notice can be read in "/info/copyright.txt".
+*
+*  Developers Group:	Costas Tsibanis <k.tsibanis@noc.uoa.gr>
+*			Yannis Exidaridis <jexi@noc.uoa.gr>
+*			Alexandros Diamantidis <adia@noc.uoa.gr>
+*			Tilemachos Raptis <traptis@noc.uoa.gr>
+*
+*  For a full list of contributors, see "credits.txt".
+*
+*  Open eClass is an open platform distributed in the hope that it will
+*  be useful (without any warranty), under the terms of the GNU (General
+*  Public License) as published by the Free Software Foundation.
+*  The full license can be read in "/info/license/license_gpl.txt".
+*
+*  Contact address: 	GUnet Asynchronous eLearning Group,
+*  			Network Operations Center, University of Athens,
+*  			Panepistimiopolis Ilissia, 15784, Athens, Greece
+*  			eMail: info@openeclass.org
+* =========================================================================*/
+
+// New titles for table accueil, used during upgrade
+// Warning: This file must be encoded in ISO-8859-7, not UTF-8!
+
+$langDropBox = array('greek' => "ÁíôáëëáãÞ Áñ÷åßùí", 'english' => "DropBox");
+$langCourseAdmin = array('greek' => "Äéá÷åßñéóç ÌáèÞìáôïò", 'english' => "Course Admin");
+$langUsers = array('greek' => "Äéá÷åßñéóç ×ñçóôþí", 'english' => "Users Admin");
+$langForums = array('greek' => "Ðåñéï÷Þ ÓõæçôÞóåùí", 'english' => "Forum");
+$langWork = array('greek' => "Åñãáóßåò", 'english' => "Assignments");
+$langWiki = array('greek' => "Óýóôçìá Wiki", 'english' => "Wiki");
+$langToolManagement = array('greek' => "Åíåñãïðïßçóç Åñãáëåßùí", 'english' => "Tools Management");
+$langCourseStat = array('greek' => "ÓôáôéóôéêÜ ×ñÞóçò", 'english' => "Usage Statistics");
+$langQuestionnaire = array('greek' => "Åñùôçìáôïëüãéï", 'english' => "Questionnaire");
+$langConference = array('greek' => "Ôçëåóõíåñãáóßá", 'english' => "Teleconference");
+$langLearnPath = array('greek' => "ÃñáììÞ ÌÜèçóçò", 'english' => "Learning Path");
+$langExercises = array('greek' => "ÁóêÞóåéò", 'english' => "Exercises");
+
